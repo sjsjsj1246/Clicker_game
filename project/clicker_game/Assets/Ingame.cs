bcf96dd4f831feff159dfad8b0f5e3d863b45c96
Hwang In Seo <sjsjsj1246@gmail.com>
2019-07-06 06:03:47 +0900

    update

---
+++ project/clicker_game/Assets/Ingame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ingame : MonoBehaviour
+{
+    int myNum;
+    public int pikaNum;
+    public GameObject ball;
+    public GameObject pikachu;
+    public Text numTxt;
+
+    void Start()
+    {
+        Debug.Log("Hello World!!");
+        pikaNum = 20;
+        numTxt.text="0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        //Debug.Log("Click!!");
+        myNum++;
+        numTxt.text = myNum.ToString();
+        Debug.Log("myname : " + myNum);
+
+        if(myNum == pikaNum)
+        {
+            pikachu.SetActive(true);
+            ball.SetActive(false);
+        }
+    }
+}
